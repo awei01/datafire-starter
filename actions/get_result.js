5c699f8074e1c46d6b824308e7402b3f5f29cc15
DataFire <info@datafire.io>
2019-02-15 18:02:19 -0500

    Edit DataFire project

--- actions/get_result.js
+++ actions/get_result.js
@@ -4,7 +4,9 @@ let datafire = require('datafire');
 let myActions = datafire.Project.main().actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let result = await myActions.return_result.run({}, context);
+    let result = await myActions.return_result.run({
+      name: "Fred",
+    }, context);
     return "Party on '" + result + "'";
   },
 });
