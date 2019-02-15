994f349fa8c07c9cfe4abd7cd9e5afdf4b820d07
DataFire <info@datafire.io>
2019-02-15 18:01:11 -0500

    Edit DataFire project

---
+++ actions/get_result.js
@@ -0,0 +1,10 @@
+"use strict";
+let datafire = require('datafire');
+
+let myActions = datafire.Project.main().actions;
+module.exports = new datafire.Action({
+  handler: async (input, context) => {
+    let result = await myActions.return_result.run({}, context);
+    return "Party on '" + result + '"';
+  },
+});
